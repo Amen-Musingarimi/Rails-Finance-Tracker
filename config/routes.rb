295ed2d4236c165f9840b471bbb127197ed7b168
Amen-Musingarimi <takudzwamusinga@gmail.com>
2022-12-14 14:04:54 +0200

    Initial commit of app

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # get 'welcome/index'
+  root 'welcome#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
